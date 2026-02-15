73b24fa431e59ec2bf16a2bcbfd18c309145443c
ismail939 <ismailsalama526@gmail.com>
2026-02-15 21:48:43 +0200

    all g"

--- HRsystem/Models/HRDepartment.cs
+++ HRsystem/Models/HRDepartment.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using HRsystem.Models;
+namespace HRsystem.Models;
 
 public class HRDepartment
 {
