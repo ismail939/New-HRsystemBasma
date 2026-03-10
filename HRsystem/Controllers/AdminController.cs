e77f378ff13f3e0ca4ef1f51742d00d73ff3cf02
ismail939 <ismailsalama526@gmail.com>
2026-03-10 23:43:31 +0200

    djfdjfdfj

--- HRsystem/Controllers/AdminController.cs
+++ HRsystem/Controllers/AdminController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 using System.IO.Compression;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Specialized;
+using HRsystem.ViewModels;
 
 namespace HRsystem.Controllers
 {
@@ -37,45 +38,99 @@ namespace HRsystem.Controllers
         {
             var listD = _context.HRDepartments.ToList();
             List<DepartmentViewModel> list = [];
-            foreach(var department in listD)
+            foreach (var department in listD)
             {
+                DepartmentViewModel depVM = null;
                 // get managerName
-                var emp = _context.HREmployees.FirstOrDefault(e=>e.Id == department.ManagerId);
-                if(emp == null)
+                if (department.ManagerId == null)
                 {
-                    return Json(new {success= false, Message = "No employee with this id was found"});
+                    depVM = new DepartmentViewModel
+                    {
+                        Name = department.Name,
+                        Code = department.Code,
+                        Description = department.Description ?? "",
+                        ManagerName = "",
+                        ParentDepartmentName = "",
+                        ManagerId = null,
+                        ParentDepartmentId = department.ParentDepartmentId,
+                    };
                 }
-                var ManagerName = emp.Name;
-                // get parentDepartmentName
-                var parentDepartment = _context.HRDepartments.FirstOrDefault(d=>d.Id == department.ParentDepartmentId);
-               
-                var ParentDepartmentName = (parentDepartment!=null)?parentDepartment.Name: "";
-                DepartmentViewModel depVM = new DepartmentViewModel
+                else
                 {
-                  Name = department.Name,
-                  Code = department.Code,
-                  Description = department.Description?? "",
-                  ManagerName = ManagerName,
-                  ParentDepartmentName = ParentDepartmentName,
-                  ManagerId = department.ManagerId,
-                  ParentDepartmentId = department.ParentDepartmentId,
-                };
+                    var emp = _context.HREmployees.FirstOrDefault(e => e.Id == department.ManagerId);
+                    if (emp == null)
+                    {
+                        return Json(new { success = false, Message = "No employee with this id was found" });
+                    }
+                    var ManagerName = emp.Name;
+                    // get parentDepartmentName
+                    var parentDepartment = _context.HRDepartments.FirstOrDefault(d => d.Id == department.ParentDepartmentId);
+
+                    var ParentDepartmentName = (parentDepartment != null) ? parentDepartment.Name : "";
+                    depVM = new DepartmentViewModel
+                    {
+                        Name = department.Name,
+                        Code = department.Code,
+                        Description = department.Description ?? "",
+                        ManagerName = ManagerName,
+                        ParentDepartmentName = ParentDepartmentName,
+                        ManagerId = department.ManagerId,
+                        ParentDepartmentId = department.ParentDepartmentId,
+                    };
+                }
+
+
                 list.Add(depVM);
             }
             return View("Departments", list);
         }
+        [HttpGet]
+        [Route("/admin/employees")]
+        public IActionResult Employees()
+        {
+            Console.WriteLine("Entered ListEmployees action");
+            var employees = _context.HREmployees.ToList();
 
-        // [HttpGet]
-        // [Route("/Admin/GetDepartmentEmployees")]
-        // public IActionResult GetDepartmentEmployees(int departmentId)
-        // {
-        //     var employees = _context.HREmployees
-        //         .Where(e => e.DepartmentId == departmentId)
-        //         .Select(e => new { Id = e.Id, Name = e.Name })
-        //         .ToList();
+            var employeeVMs = new List<EmployeeViewModel>();
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"Processing employee: {emp.Name} with department ID: {emp.HRDepartmentId}");
+                var dep = _context.HRDepartments.FirstOrDefault(d => d.Id == emp.HRDepartmentId);
+                string depName = dep != null ? dep.Name : "";
+                employeeVMs.Add(new EmployeeViewModel
+                {
+                    Id = emp.Id,
+                    Name = emp.Name,
+                    NationalId = emp.NationalId,
+                    PhoneNumber = emp.PhoneNumber,
+                    MarriageStatus = emp.MarriageStatus,
+                    Religion = emp.Religion,
+                    DateOfBirth = emp.DateOfBirth,
+                    InsuranceNumber = emp.InsuranceNumber,
+                    HireDate = emp.HireDate,
+                    EndDate = emp.EndDate,
+                    JobName = emp.JobName,
+                    ContractType = emp.ContractType,
+                    LeaveReason = emp.LeaveReason,
+                    BasmaId = emp.BasmaId,
+                    HRDepartmentId = emp.HRDepartmentId,
+                    Department = depName == "" ? "" : depName
+                });
+            }
 
-        //     return Json(employees);
-        // }
+            return View("Employees", employeeVMs);
+        }
+        [HttpGet]
+        [Route("/Admin/GetDepartmentEmployees")]
+        public IActionResult GetDepartmentEmployees(int departmentId)
+        {
+            var employees = _context.HREmployees
+                .Where(e => e.HRDepartmentId == departmentId)
+                .Select(e => new { Id = e.Id, Name = e.Name })
+                .ToList();
+
+            return Json(employees);
+        }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -115,7 +170,7 @@ namespace HRsystem.Controllers
             _context.HRDepartments.Add(department);
             await _context.SaveChangesAsync();
 
-            return RedirectToAction("CreateForm");
+            return RedirectToAction("Departments");
         }
         [HttpGet]
         [Route("/createDepartment")]
@@ -143,6 +198,30 @@ namespace HRsystem.Controllers
             return View("CreateDepartment", model);
         }
 
+        [HttpPost]
+        public IActionResult EditDepartment(int Id,
+            string Name,
+            string Code,
+            string Description,
+            int parentDepartmentId,
+            int managerId)
+        {
+            // Console.WriteLine($"here are the values id: {Id} name: {Name} code: {Code} description: {Description} parentDepId: {parentDepartmentId} managerId: {managerId}");
+            // get the department
+            var department = _context.HRDepartments.FirstOrDefault(d=>d.Id==Id);
+            if (department == null)
+            {
+                return NotFound(); 
+            }
+            department.Name = Name;
+            department.Code = Code;
+            department.Description = Description;
+            department.ParentDepartmentId = parentDepartmentId;
+            department.ManagerId = managerId;
+            _context.HRDepartments.Update(department);
+            _context.SaveChanges();
+            return RedirectToAction("Departments");
+        }
 
     }
 }
